1e73c0775eada2d7ba7d45afd3510fac3152fcee
SergeyLankevich <43179675+SergeyLankevich@users.noreply.github.com>
2020-02-27 23:12:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import turtle as t
 import math
+import tkinter
+from tkinter import ttk
+import functools
 
 
 def square(n, side):
@@ -63,25 +66,25 @@ def ice2(n, side):
         ice2(n - 1, side/2)
 
 
-def minkovski(n, side):
+def minkowski(n, side):
     t.down()
     if n == 0:
         t.fd(side)
     else:
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.lt(90)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.rt(90)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.rt(90)
-        minkovski(n - 1, side / 4)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.lt(90)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.lt(90)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
         t.rt(90)
-        minkovski(n - 1, side / 4)
+        minkowski(n - 1, side / 4)
 
 
 def levi(n, side):
@@ -95,19 +98,39 @@ def levi(n, side):
         levi(n - 1, 1 / math.sqrt(2) * side)
         t.lt(45)
 
-def tree(n, side, det):
+
+def tree(n, side):
     t.down()
     if n == 0:
-        t.fd(side * det)
-        t.bk(side * det)
+        t.fd(side * 0.75)
+        t.bk(side * 0.75)
     else:
         t.fd(side)
         t.rt(30)
-        tree(n - 1, side * det, det)
+        tree(n - 1, side * 0.75)
         t.lt(60)
-        tree(n - 1, side * det, det)
+        tree(n - 1, side * 0.75)
         t.rt(30)
         t.bk(side)
+
+
+def dragon(n, side):
+    t.speed(0)
+    if n == 0:
+        return t.forward(side)
+    t.right(45)
+    dragon(n - 1, side * 1 / 2 ** (1 / 2))
+    t.left(90)
+    t.up()
+    t.forward(side * 1 / 2 ** (1 / 2))
+    t.down()
+    t.left(180)
+    dragon(n - 1, side * 1 / 2 ** (1 / 2))
+    t.left(180)
+    t.up()
+    t.forward(side * 1 / 2 ** (1 / 2))
+    t.down()
+    t.right(45)
 
 
 def main_koch(n, side):
@@ -176,11 +199,11 @@ def main_ice2(n, side):
     t.rt(120)
 
 
-def main_minkovski(n, side):
+def main_minkowski(n, side):
     t.speed(0)
     t.up()
     t.setx(-side/2)
-    minkovski(n, side)
+    minkowski(n, side)
 
 
 def main_levi(n, side):
@@ -190,7 +213,7 @@ def main_levi(n, side):
     levi(n, side)
 
 
-def main_tree(n, side, det):
+def main_tree(n, side):
     y = 0
     l = side
     for i in range(n + 1):
@@ -198,67 +221,56 @@ def main_tree(n, side, det):
             y += l
         else:
             y += l * math.sqrt(3)/2
-        l *= det
+        l *= 0.75
 
     t.speed(0)
     t.up()
     t.sety(-y/2)
     t.setheading(90)
-    tree(n, side, det)
+    tree(n, side)
 
 
-print('What do you want to draw (choose the number)?')
-print('1. Koch Snowflake')
-print('2. Ice Pattern 1')
-print('3. Ice Pattern 2')
-print('4. Minkovski Fractal')
-print('5. Levi Fractal')
-print('6. Binary Tree')
-print('7. Fractal Dragon')
-choice = input()
-while True:
-    try:
-        choice = int(choice)
-    except ValueError:
-        print('Please, enter the number')
-    else:
-        if 1 <= choice <= 7:
-            break
-        else:
-            print('Please, enter the number from the proposal')
-            choice = input()
-n = input('n = ')
-while True:
-    try:
-        n = int(n)
-    except ValueError:
-        print('Please, enter the natural number')
-        n = input('n = ')
-    else:
-        break
-side = input('side = ')
-while True:
-    try:
-        side = int(side)
-    except ValueError:
-        print('Please, enter the natural number')
-        side = input('side = ')
-    else:
-        break
+def scales(figures, index, depth, length):
+    recursion_depth = depth.get()
+    side_length = length.get()
+    figures.get(index)(recursion_depth, side_length)
+    print(recursion_depth, side_length)
 
-if choice == 1:
-    main_koch(n, side)
-elif choice == 2:
-    main_ice(n, side)
-elif choice == 3:
-    main_ice2(n, side)
-elif choice == 4:
-    main_minkovski(n, side)
-elif choice == 5:
-    main_levi(n, side)
-elif choice == 6:
-    main_tree(n, side, 1/2)
-elif choice == 7:
-    print('Sorry, the dragon got lost somewhere. Now we will find him...')
 
-t.exitonclick()
+figuresDict = {'Koch Snowflake': main_koch, 'Ice Pattern 1': main_ice, 'Ice Pattern 2': main_ice2,
+               'Minkowski Fractal': main_minkowski, 'Levi Fractal': main_levi, 'Binary Tree': main_tree,
+               'Dragon': dragon}
+
+root = tkinter.Tk()
+mainFrame = ttk.Frame(root)
+mainFrame.pack(side=tkinter.LEFT, fill='both', expand=True)
+
+addFrame = ttk.Frame(root)
+addFrame.pack(side=tkinter.RIGHT, fill='both', expand=True)
+
+depthLabel = tkinter.Label(addFrame, text="Recursion depth: ")
+depthLabel.pack()
+depthScale = tkinter.Scale(addFrame, orient=tkinter.HORIZONTAL, length=1000, from_=0, to=80, tickinterval=5,
+                           resolution=1)
+depthScale.pack()
+
+lengthLabel = tkinter.Label(addFrame, text="Side length: ")
+lengthLabel.pack()
+lengthScale = tkinter.Scale(addFrame, orient=tkinter.HORIZONTAL, length=1000, from_=0, to=1000, tickinterval=50,
+                            resolution=5)
+lengthScale.pack()
+
+n = depthScale.get()
+side = lengthScale.get()
+
+resetButton = ttk.Button(mainFrame, text='Reset', width=18, command=t.bye)
+resetButton.pack()
+
+for key in figuresDict.keys():
+    button = ttk.Button(mainFrame, text=key, width=18,
+                        command=functools.partial(scales, figures=figuresDict, index=key,
+                                                  depth=depthScale,
+                                                  length=lengthScale))
+    button.pack(side=tkinter.BOTTOM)
+
+root.mainloop()
